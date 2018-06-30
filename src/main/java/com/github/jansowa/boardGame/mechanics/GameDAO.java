f91c20f74647cfbffc8b31dac777e10b588b88f6
Jan Sowa <jan.piotr.sowa@gmail.com>
2018-06-30 11:41:50 +0200

    Add Spring Boot to pom.xml, fix GameDAO (change type of argument in loadGame function), new JAR

--- src/main/java/com/github/jansowa/boardGame/mechanics/GameDAO.java
+++ src/main/java/com/github/jansowa/boardGame/mechanics/GameDAO.java
@@ -4,5 +4,5 @@ import com.github.jansowa.boardGame.domain.GameBoard;
 
 public abstract class GameDAO {
 	public abstract GameBoard loadGame(String gameName);
-	public abstract void saveGame(String gameName);
+	public abstract void saveGame(GameBoard board);
 }
